0b18f6cce7439df48034cc5e48c7c30b4e1372d3
Debika Sarkar <debikasarkar713@gmail.com>
2016-12-07 11:32:07 -0500

    added landing search and side search components

---
+++ src/components/App/SideSearch/SideSearch.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './SideSearch.css';
+
+class SideSearch extends Component {
+  render(){
+    return (
+      )
+  }
+}
+
+export default SideSearch;
+
