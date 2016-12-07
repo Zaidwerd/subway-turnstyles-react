6c95395faed371702c8a9f1d1d39d0ccb8ad6e69
Krystyna Malewski <krmalewski@gmail.com>
2016-12-07 11:26:12 -0500

    Add MapContainer component to App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import MapContainer from '../MapContainer/MapContainer.jsx';
 
 export default class App extends React.Component{
-  render(){
-    return(
+  render() {
+    return (
       <container>
         <h1> Subways and turnstyles! </h1>
+        <MapContainer />
       </container>
-    )
+    );
   }
 }
