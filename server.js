a6ab8586ea528e7b375c8ddeefd4b204908c9930
Scott McCord <scott.sdm27@gmail.com>
2016-12-07 10:59:47 -0500

    react server completed

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// require our packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+
+// const homeRoute = require('./routes/index.js')
+
+// Only load dotenv if we need it (if we have NODE_ENV in our environment)
+const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
+
+const app = express();
+const PORT = process.argv[2] || process.env.port || 3000;
+
+// log resonse code
+app.use(logger(isDev ? 'dev' : 'common'));
+
+// accept JSON formatted data
+app.use(bodyParser.json());
+
+// handle the routes
+// app.use('/', homeRoute);
+
+// add an error handler
+app.use((err, req, res, next) => {
+  console.error(err, next);
+  res.status(500).send('Something broke!');
+});
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// start server
+app.listen(PORT);
+
---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <container>
+        <h1> Subways and turnstyles! </h1>
+      </container>
+    )
+  }
+}
